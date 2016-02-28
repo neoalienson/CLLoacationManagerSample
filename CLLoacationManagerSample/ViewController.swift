b063d4cff0d22ab66ecff79cba6be8c7d17c6bb5
Neo <neo@orz.hk>
2016-02-28 17:37:24 +0800

    Initial Commit

---
+++ CLLoacationManagerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CLLoacationManagerSample
+//
+//  Created by Neo on 2/28/16.
+//  Copyright © 2016 padblish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
